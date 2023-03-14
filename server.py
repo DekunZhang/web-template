8db6b5b8aa6c1f82c165303d08c081248a2dd1e6
Dekun Zhang <91729508+DekunZhang@users.noreply.github.com>
2023-03-14 07:49:08 +0000

    feat: add html template

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import web
 urls = (
     '/', 'index',
     '/json', 'json_handler',
+    '/html', 'html_handler'
 )
 
 
@@ -14,16 +15,16 @@ class index:
         message = data.get('message')
         name = data.get('name')
         age = data.get('age')
-        return f"Hello {name}, you are {age} years old and you sent a message: " \
-               f"{message}"
+        return f"Hello {name}, you are {age} years old and you sent a " \
+               f"message: {message}"
 
     def POST(self):
         data = web.input()
         message = data.get('message')
         name = data.get('name')
         age = data.get('age')
-        return f"Hello {name}, you are {age} years old and you sent a message: " \
-               f"{message}"
+        return f"Hello {name}, you are {age} years old and you sent a " \
+               f"message: {message}"
 
 
 class json_handler:
@@ -37,6 +38,20 @@ class json_handler:
         json_data = json.loads(data)
         message = json_data['message']
         return f'Received message: {message}'
+
+
+class html_handler:
+    def GET(self):
+        web.header('Content-Type', 'text/html; charset=UTF-8')
+        return '''<!DOCTYPE html>
+        <html>
+        <head>
+          <title>Example</title>
+        </head>
+        <body>
+          <h1>Hello, World!</h1>
+        </body>
+        </html>'''
 
 
 if __name__ == "__main__":
